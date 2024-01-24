3fb957f65f4a62de586efb70a2c7e39451de12c2
Aarush <157492561+AarushSachdeva@users.noreply.github.com>
2024-01-24 10:21:36 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-#Make a basic calculator which performs addition,subtraction,multiplication,division,floor division,exponential on two numbers.
+#Make a simple calculator with basic arithmetic operations.Prompt the user to input two numbers and an operation choice.Perform the calculation and display the result.
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 choice=int(input("Enter the operation you want to perform\nPress 1 for sum\nPress 2 for subtraction\nPress 3 for multiplication\nPress 4 for division\nPress 5 for floor division\nPress 6 for exponential power\n"))
@@ -6,7 +6,7 @@ match choice:
   case 1:
     print("The sum of the numbers is",a+b)
   case 2:
-    choice=int(input("Press 1 for a-b\nPress 2 for b-a"))
+    choice=int(input("Press 1 for a-b\nPress 2 for b-a\n"))
     match choice:
       case 1:
         print("The subtraction of a and b is",a-b)
@@ -15,21 +15,33 @@ match choice:
   case 3:
     print("The multiplication of a and b is",a*b)
   case 4:
-    choice=int(input("Press 1 for a/b\nPress 2 for b/a"))
+    choice=int(input("Press 1 for a/b\nPress 2 for b/a\n"))
     match choice:
        case 1:
-         print("The division of a by b is",a/b)
+         if(b!=0):
+           print("The division of a by b is",a/b)
+         else:
+           print("Division by 0 is not valid operation")
        case 2:
-         print("The division of b by a is",b/a)
+         if(a!=0):
+           print("The division of b by a is",b/a)
+         else:
+           print("Division by 0 is not valid operation")
   case 5:
-    choice=int(input("Press 1 for a//b\nPress 2 for b//a"))
+    choice=int(input("Press 1 for a//b\nPress 2 for b//a\n"))
     match choice:
        case 1:
-         print("The floor division of a by b is",a//b)
+         if(b!=0):
+           print("The floor division of a by b is",a//b)
+         else:
+           print("Floor division by 0 is not valid operation")
        case 2:
-         print("The floor division of b by a is",b//a)
+         if(a!=0):
+           print("The floor division of b by a is",b//a)
+         else:
+           print("Floor division by 0 is not valid operation")
   case 6:
-     choice=int(input("Press 1 for a**b\nPress 2 for b**a"))
+     choice=int(input("Press 1 for a**b\nPress 2 for b**a"\n))
      match choice:
        case 1:
          print("The exponential of a to b is",a**b)
